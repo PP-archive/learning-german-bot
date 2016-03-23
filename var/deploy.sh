e0cdf479a558e987e5e6e842f61ee73137710366
Pavel Polyakov <me@pavelpolyakov.com>
2016-03-23 22:10:49 +0100

    deploy integration

---
+++ var/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+npm i
+npm prune
+
+# pm2 launch or restart
+currentlyActive=$(pm2 list | grep "bot" | grep online)
+echo $currentlyActive
+
+# do the start if the length is 0
+if [ -z "$currentlyActive" ]
+    then
+    NODE_ENV=production DEBUG=* pm2 start index.js -n bot -i 0 -f
+else
+    pm2 restart bot
+fi
+
+echo "Deploy is finished"
